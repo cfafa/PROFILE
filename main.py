d1110cc9d3ebea033a57b9fb8697b1a9006be9d7
cfafa <143934336+cfafa@users.noreply.github.com>
2023-12-24 14:09:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,13 @@ def process_form():
     button_python = request.form.get('button_python')
     return render_template('index.html', button_python=button_python)
 
-def process_form():
     button_discord = request.form.get('button_discord')
     return render_template('index.html', button_discord=button_discord)
 
 
-def process_form():
     button_html = request.form.get('button_html')
     return render_template('index.html', button_html=button_html)
 
-def process_form():
     button_db = request.form.get('button_db')
     return render_template('index.html', button_db=button_db)
 
